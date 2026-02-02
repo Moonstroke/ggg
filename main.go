2cc14daafd4c4cc619167580dee0e3e50febf362
Joachim MARIE <moonstroke+github@live.fr>
2026-02-02 13:17:26 +0100

    Be stricter on what the command line accepts
    If one is given, the command-line parameter is now compared to both host
    and join before choosing the fucntion to which delegate; if any other
    value is passed, the program exits with error.
    As before, an unspecified parameter leads to a join.

--- main.go
+++ main.go
@@ -9,10 +9,17 @@ import (
 
 func main() {
 	/* If requested, set up a game; by default, look for one instead */
-	if len(os.Args) > 1 && os.Args[1] == "host" {
-		hostGame()
-	} else {
+	if len(os.Args) == 1 {
 		joinGame()
+	} else if os.Args[1] == "host" {
+		hostGame()
+	} else if os.Args[1] == "join" {
+		joinGame()
+	} else {
+		os.Stderr.WriteString("Usage: ggg ACTION?\n")
+		os.Stderr.WriteString("\twhere ACTION is either \"host\" or \"join\"\n")
+		os.Stderr.WriteString("\tif ACTION is unspecified, the default is \"join\"\n")
+		os.Exit(1)
 	}
 }
 
