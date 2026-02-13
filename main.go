0e6bfc322e494eb55da1b486d73c47700cbe74f9
Joachim MARIE <moonstroke+github@live.fr>
2026-02-13 22:25:32 +0100

    Start actual communication between host/players
    Joiners write a (dummy) message to the connection every second (rate
    throttled to not flood the network), while the host reads them at the
    other end.

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 const DEFAULT_ADDRESS = ":10042"
+const BUFFER_SIZE = 256
 
 var DEBUG = log.New(os.Stderr, "[DEBUG] ", log.Lshortfile)
 var ERROR = log.New(os.Stderr, "[ERROR] ", log.LstdFlags|log.Lshortfile)
@@ -53,7 +55,15 @@ func hostGame() {
 	DEBUG.Println(conn.LocalAddr(), "is connected to", conn.RemoteAddr())
 	defer conn.Close()
 
-	// TODO wait for players to make themselves known => read
+	buffer := make([]byte, BUFFER_SIZE)
+	for {
+		n, addr, err := conn.ReadFrom(buffer)
+		if err != nil {
+			ERROR.Fatalln(err)
+		}
+		DEBUG.Println("Received", string(buffer[:n]), "from", addr)
+		// TODO if addr is a player, register and notify it
+	}
 }
 
 func joinGame() {
@@ -68,5 +78,13 @@ func joinGame() {
 	DEBUG.Println(conn.LocalAddr(), "is connected to", conn.RemoteAddr())
 	defer conn.Close()
 
-	// TODO speak to hosts on network => write
+	payload := []byte("I wanna play!")
+	for {
+		_, err := conn.Write(payload)
+		if err != nil {
+			ERROR.Fatalln(err)
+		}
+		time.Sleep(1 * time.Second)
+		// TODO if host replies, stop sending
+	}
 }
