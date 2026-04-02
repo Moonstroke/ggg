77cb91e6efcbb34f76491c483383de8449e9d7a6
Joachim MARIE <moonstroke+github@live.fr>
2026-04-02 22:58:19 +0200

    Extract communication code into distinct functions
    The operations of sending and receiving a join request or acknowledgement are
    moved out of the hostGame and joinGame functions to keep these simpler.

--- main.go
+++ main.go
@@ -73,6 +73,28 @@ func main() {
 	}
 }
 
+func recvJoinRequest(conn *net.UDPConn, buffer []byte) (string, *net.UDPAddr) {
+	n, addr, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		ERROR.Fatalln(err)
+	}
+	msg := string(buffer[:n])
+	DEBUG.Println("Received", msg, "from", addr)
+	var playerName string
+	if n, err = fmt.Sscanf(msg, JOIN_MSG_FMT, &playerName); err != nil {
+		ERROR.Println(err)
+		return "", nil
+	}
+	if n != 1 {
+		return "", nil
+	}
+	return playerName, addr
+}
+
+func sendJoinAck(conn *net.UDPConn, addr *net.UDPAddr, name, playerName string) {
+	conn.WriteToUDP(fmt.Appendf(nil, ACCEPT_MSG_FMT, name, playerName), addr)
+}
+
 func hostGame(name string, playerCount int) {
 	players := make([]player, 0, playerCount)
 	DEBUG.Println("Hosting game")
@@ -87,27 +109,46 @@ func hostGame(name string, playerCount int) {
 	players = append(players, player{name, conn.LocalAddr()})
 	buffer := make([]byte, BUFFER_SIZE)
 	for {
-		n, addr, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			ERROR.Fatalln(err)
-		}
-		msg := string(buffer[:n])
-		DEBUG.Println("Received", msg, "from", addr)
-		var playerName string
-		if n, err = fmt.Sscanf(msg, JOIN_MSG_FMT, &playerName); err != nil {
-			ERROR.Println(err)
+		playerName, addr := recvJoinRequest(conn, buffer)
+		if addr == nil {
 			continue
 		}
-		if n == 1 {
-			/* Message was a join request: accept player */
-			DEBUG.Println("Acepting player", playerName)
-			conn.WriteToUDP(fmt.Appendf(nil, ACCEPT_MSG_FMT, name, playerName), addr)
-			players = append(players, player{playerName, addr})
-			if len(players) == playerCount {
-				break
-			}
+		DEBUG.Println("Acepting player", playerName)
+		sendJoinAck(conn, addr, name, playerName)
+		players = append(players, player{playerName, addr})
+		if len(players) == playerCount {
+			break
 		}
 	}
+}
+
+func sendJoinRequest(conn *net.UDPConn, name string) {
+	payload := fmt.Appendf(nil, JOIN_MSG_FMT, name)
+	if _, err := conn.Write(payload); err != nil {
+		ERROR.Fatalln(err)
+	}
+}
+
+func recvJoinAck(conn *net.UDPConn, buffer []byte, replyFmt string) string {
+	var n int
+	var err error
+	if n, _, err = conn.ReadFromUDP(buffer); err != nil {
+		ERROR.Println(err)
+		return ""
+	}
+	reply := string(buffer[:n])
+	DEBUG.Println("Received", reply, "from", conn.RemoteAddr())
+	var hostName string
+	if n, err = fmt.Sscanf(reply, replyFmt, &hostName); err != nil {
+		ERROR.Println(err)
+		return ""
+	}
+	if n == 1 {
+		/* Host accepted our request */
+		DEBUG.Println("Join request accepted by host", hostName, "@", conn.RemoteAddr())
+		return hostName
+	}
+	return ""
 }
 
 func joinGame(name string) {
@@ -122,31 +163,14 @@ func joinGame(name string) {
 	DEBUG.Println(conn.LocalAddr(), "is connected to", conn.RemoteAddr())
 	defer conn.Close()
 
-	payload := fmt.Appendf(nil, JOIN_MSG_FMT, name)
 	buffer := make([]byte, BUFFER_SIZE)
 	/* Dirty hack, but the only way I found to format only one flag */
 	replyFmt := fmt.Sprintf(ACCEPT_MSG_FMT, "%s", name)
 	for {
-		_, err := conn.Write(payload)
-		if err != nil {
-			ERROR.Fatalln(err)
-		}
+		sendJoinRequest(conn, name)
 		conn.SetReadDeadline(time.Now().Add(time.Second))
-		var n int
-		if n, _, err = conn.ReadFromUDP(buffer); err != nil {
-			ERROR.Println(err)
-			continue
-		}
-		reply := string(buffer[:n])
-		DEBUG.Println("Received", reply, "from", conn.RemoteAddr())
-		var hostName string
-		if n, err = fmt.Sscanf(reply, replyFmt, &hostName); err != nil {
-			ERROR.Println(err)
-			continue
-		}
-		if n == 1 {
-			/* Host accepted our request */
-			DEBUG.Println("Join request accepted by host", hostName, "@", conn.RemoteAddr())
+		hostName := recvJoinAck(conn, buffer, replyFmt)
+		if hostName != "" {
 			break
 		}
 	}
