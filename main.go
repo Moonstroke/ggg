d4fbffdbc9a15a11aa79933997dc0d75c34d458a
Joachim MARIE <moonstroke+github@live.fr>
2026-05-30 18:52:47 +0200

    Ensure a IPv4 address is used

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func findCurrentNetAddr() (net.IP, error) {
 		DEBUG.Println("Addresses of interface", intf, ":", addrs)
 		if intf.Flags & ^net.FlagLoopback & net.FlagRunning != 0 {
 			for _, addr := range addrs {
-				if ip, ok := addr.(*net.IPNet); ok && !ip.IP.IsLoopback() {
+				if ip, ok := addr.(*net.IPNet); ok && !ip.IP.IsLoopback() && ip.IP.To4() != nil {
 					return ip.IP, nil
 				}
 			}
